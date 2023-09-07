614ce3d46c7be06141b9988785b9b54bb957c11e
Silo <s.lotfi@emofid.com>
2023-09-07 19:06:41 +0330

    feat(directives): add two different directives to observe html elements

--- src/core/directives/image-observe/image-observe.directive.ts
+++ src/core/directives/image-observe/image-observe.directive.ts
@@ -1,6 +1,61 @@
-import { Directive } from '@angular/core';
+import { DestroyRef, Directive, ElementRef, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
+
+interface IntersectionEntry {
+	isAppearing: boolean;
+	isDisappearing: boolean;
+}
 
 @Directive({
-	selector: '[appImageObserve]'
+	selector: '[imageObserve]'
 })
-export class ImageObserveDirective {}
+export class ImageObserveDirective implements OnInit {
+	@Input({ required: false }) root?: null;
+	@Input({ required: false }) margin?: '0px';
+	@Input({ required: false }) threshold = 0.1;
+	@Output() isAppearing = new EventEmitter<boolean>();
+	@Output() isDisappearing = new EventEmitter<boolean>();
+
+	private destroyer = inject(DestroyRef);
+
+	constructor(private element: ElementRef) {}
+
+	ngOnInit(): void {
+		this.observe()
+			.pipe(takeUntilDestroyed(this.destroyer))
+			.subscribe((entry: IntersectionEntry) => {
+				const { isAppearing, isDisappearing } = entry;
+				this.isAppearing.emit(isAppearing);
+				this.isDisappearing.emit(isDisappearing);
+			});
+	}
+
+	private observe(): Observable<IntersectionEntry> {
+		const options: IntersectionObserverInit = {
+			root: this.root,
+			rootMargin: '0px',
+			threshold: this.threshold
+		};
+
+		return new Observable<IntersectionEntry>(subscriber => {
+			const intersectionObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+				const { isIntersecting, intersectionRatio } = entries[0];
+
+				const entry = {
+					isAppearing: isIntersecting && intersectionRatio > this.threshold,
+					isDisappearing: !isIntersecting && intersectionRatio < this.threshold
+				} as IntersectionEntry;
+				subscriber.next(entry);
+			}, options);
+
+			intersectionObserver.observe(this.element.nativeElement);
+
+			return {
+				unsubscribe() {
+					intersectionObserver.disconnect();
+				}
+			};
+		});
+	}
+}
--- src/core/directives/loader-observe/loader-observe.directive.ts
+++ src/core/directives/loader-observe/loader-observe.directive.ts
@@ -1,6 +1,48 @@
-import { Directive } from '@angular/core';
+import { DestroyRef, Directive, ElementRef, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
 
 @Directive({
-	selector: '[appLoaderObserve]'
+	selector: '[loaderObserve]'
 })
-export class LoaderObserveDirective {}
+export class LoaderObserveDirective implements OnInit {
+	@Input({ required: false }) root? = null;
+	@Input({ required: false }) margin? = '0px';
+	@Input({ required: false }) threshold = 0.1;
+	@Output() isVisible = new EventEmitter<void>();
+
+	private destroyer = inject(DestroyRef);
+
+	constructor(private element: ElementRef) {}
+
+	ngOnInit(): void {
+		this.observe()
+			.pipe(takeUntilDestroyed(this.destroyer))
+			.subscribe(() => {
+				this.isVisible.emit();
+			});
+	}
+
+	private observe(): Observable<void> {
+		const options: IntersectionObserverInit = {
+			root: this.root,
+			rootMargin: '0px',
+			threshold: this.threshold
+		};
+
+		return new Observable<void>(subscriber => {
+			const intersectionObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+				const { isIntersecting, intersectionRatio } = entries[0];
+				if (isIntersecting && intersectionRatio > this.threshold) subscriber.next();
+			}, options);
+
+			intersectionObserver.observe(this.element.nativeElement);
+
+			return {
+				unsubscribe() {
+					intersectionObserver.disconnect();
+				}
+			};
+		});
+	}
+}
